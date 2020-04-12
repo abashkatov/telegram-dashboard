67c7ba0c2f9ae8885ee4828587ae95d06ad9b3f0
a.bashkatov <as@bashkatov.pro>
2020-04-12 19:20:03 +0300

    add security service with tests: - login, - register, - logout.

--- src/service/security/Security.js
+++ src/service/security/Security.js
@@ -1,5 +1,40 @@
+import axios from "axios";
+
 export default class Security {
-    getUser() {
-        return {};
+    constructor(options = {}) {
+        this.client = options.client || axios.create();
+        this.token = options.token;
+        this.refreshToken = options.refreshToken;
+    }
+
+    async register({username, email, password, passwordConfirm}) {
+        const {data} = await this.client.post(
+            process.env.VUE_APP_BASE_URI + '/registration',
+            {username, email, password, passwordConfirm}
+        );
+        this.token = data.token;
+        this.refreshToken = data.refreshToken;
+        this.client.defaults.headers.common['Authorization'] = `Bearer ${this.token}`;
+    }
+    async login({username, password}) {
+        const {data} = await this.client.post(
+            process.env.VUE_APP_BASE_URI + '/login',
+            {username, password}
+        );
+        this.token = data.token;
+        this.refreshToken = data.refreshToken;
+        this.client.defaults.headers.common['Authorization'] = `Bearer ${this.token}`;
+    }
+
+    logout() {
+        this.token = null;
+        this.refreshToken = null;
+        delete this.client.defaults.headers.common['Authorization'];
+    }
+
+    async getUser() {
+        return await this.client.get(
+            process.env.VUE_APP_BASE_URI + '/api/user'
+        );
     }
 }
--- src/test/service/security/index.js
+++ src/test/service/security/index.js
@@ -1,7 +1,59 @@
 import test from "ava";
 import Security from "../../../service/security/Security.js"
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 
+test.beforeEach(t => {
+    const client = axios.create();
+    t.context.mock = new MockAdapter(client);
+    t.context.security = new Security({client});
+    t.context.mock.onGet(process.env.VUE_APP_BASE_URI + '/api/user')
+        .reply(200, {});
+});
 test("Get empty user", t => {
     const security = new Security();
     t.truthy(security);
 });
+
+test("Login", async t => {
+    const LOGIN_REQUEST = {
+        username: "login",
+        password: "password",
+    };
+    const LOGIN_RESPONSE = {
+        token: "TOKEN",
+        refreshToken: "REFRESH_TOKEN",
+    };
+    t.context.mock.onPost(process.env.VUE_APP_BASE_URI + '/login', LOGIN_REQUEST)
+        .reply(200, LOGIN_RESPONSE);
+
+    await t.context.security.login(LOGIN_REQUEST);
+    await t.context.security.getUser();
+    await t.context.security.logout();
+    await t.context.security.getUser();
+
+    t.is(t.context.mock.history.get.length, 2);
+    t.is(t.context.mock.history.get[0].headers.Authorization, `Bearer ${LOGIN_RESPONSE.token}`);
+    t.falsy(t.context.mock.history.get[1].headers.Authorization);
+});
+
+test("Register", async t => {
+    const REGISTER_REQUEST = {
+        username: "LOGIN",
+        email: "EMAIL",
+        password: "PASSWORD",
+        passwordConfirm: "PASSWORD_CONFIRM",
+    };
+    const REGISTER_RESPONSE = {
+        token: "TOKEN",
+        refreshToken: "REFRESH_TOKEN",
+    };
+    t.context.mock.onPost(process.env.VUE_APP_BASE_URI + '/registration', REGISTER_REQUEST)
+        .reply(200, REGISTER_RESPONSE);
+    await t.context.security.register(REGISTER_REQUEST);
+    await t.context.security.getUser();
+    t.is(t.context.mock.history.get.length, 1);
+    t.is(t.context.mock.history.get[0].headers.Authorization, `Bearer ${REGISTER_RESPONSE.token}`);
+});
+
+
